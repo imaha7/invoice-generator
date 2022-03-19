2469972d8384dd6f0f7db48428d5e59c9df9ef17
imaha7 <ilham.maulana.07.0698@gmail.com>
2022-03-19 19:21:04 +0700

    DONE

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InvoicePage from './components/InvoicePage'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="center fs-30">Aplikasi Invoice Generator</h1>
+      <InvoicePage />
+      <p className="center">By: Ilham Maulana Habibie, S.Kom</p>
+    </div>
+  )
+}
+
+export default App
